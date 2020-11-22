765aed2d134b3cba3a66b2bfbead4df6b3a832f9
Rodrigo Yuri <rodrigo33996@gmail.com>
2020-11-22 14:07:50 -0300

    Fromatação de data e estilo da pagina de posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,21 +1,22 @@
 import Head from 'next/head'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Post({ postData }) {
     return (
       <Layout>
-        <Head>
-          <title>{postData.title}</title>
-        </Head>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-        <br />
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </Layout>
+      </article>
+    </Layout>
     )
 }
 
